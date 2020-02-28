57f108f4fb5364af1d1e2800db0bffbf67d000c7
Serhii Kamolov <serhii@sslcertificaten.nl>
2020-02-29 00:44:43 +0200

    Add custom Service Provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+namespace serhiikamolov;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // default response class
+        $this->app->bind(
+            \serhiikamolov\Laravel\JsonApi\Contracts\Response::class,
+            \serhiikamolov\Laravel\JsonApi\Response\Response::class
+        );
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
