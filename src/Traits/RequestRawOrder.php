3a5de3c15134f360889a23824c534568ad28b557
Serhii Kamolov <serhii@sslcertificaten.nl>
2020-03-03 23:57:51 +0200

    Add trait for getting a raw order from the request

---
+++ src/Traits/RequestRawOrder.php
@@ -0,0 +1,19 @@
+<?php
+namespace serhiikamolov\Laravel\JsonApi\Traits;
+
+trait RequestRawOrder {
+
+    public function getRawOrder(string $default = 'id asc'):string
+    {
+        $orderList = null;
+
+        if ($this->order) {
+            $orderList = explode(",", $this->order);
+            foreach ($orderList as $key => $value) {
+                $orderList[$key] = trim($value, "- ") . " " . ($value[0]==='-' ? 'DESC' : 'ASC');
+            }
+        }
+
+        return $orderList ? implode(", ", $orderList) : $default;
+    }
+}
