917919dc8d40711490fb0b563e45c14dfa786d53
serhiikamolov <serhii.kamolov@gmail.com>
2020-08-03 18:02:53 +0300

    Add more test asserts

--- src/Traits/Tests/JsonApiAsserts.php
+++ src/Traits/Tests/JsonApiAsserts.php
@@ -13,11 +13,19 @@ trait JsonApiAsserts
         $this->assertTrue(isset($data['data']) || isset($data['errors']));
     }
 
-    protected function assertAuthTokenResponse(JsonResponse $response)
+    protected function assertJsonApiAuthResponse(JsonResponse $response)
     {
         $data = $response->getData(true);
         $this->assertArrayHasKey('access_token', $data['data']);
         $this->assertArrayHasKey('token_type', $data['data']);
         $this->assertArrayHasKey('expires_in', $data['data']);
     }
+
+    protected function assertJsonApiResponseError(JsonResponse $response, string $error)
+    {
+        $data = $response->getData(true);
+        $this->assertArrayHasKey('errors', $data);
+        return in_array($error, $data['errors']);
+    }
+
 }
