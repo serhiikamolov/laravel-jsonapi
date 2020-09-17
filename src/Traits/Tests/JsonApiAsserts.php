6b0f22d710e80ceacfec4ba5f94947cbbef8b104
serhiikamolov <serhii.kamolov@gmail.com>
2020-09-18 00:14:50 +0300

    Update testing asserts

--- src/Traits/Tests/JsonApiAsserts.php
+++ src/Traits/Tests/JsonApiAsserts.php
@@ -1,28 +1,53 @@
 <?php
+
 namespace JsonAPI\Traits\Tests;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Testing\TestResponse;
 
 trait JsonApiAsserts
 {
-    protected function assertJsonApiResponse(JsonResponse $response)
+    /**
+     * @param JsonResponse | TestResponse $response
+     * @param array|null $fields
+     */
+    protected function assertJsonApiResponse($response, array $fields = null)
     {
+        $response = $response instanceof TestResponse ? $response->baseResponse : $response;
+
         $data = $response->getData(true);
         $this->assertArrayHasKey('jsonapi', $data);
         $this->assertArrayHasKey('links', $data);
         $this->assertTrue(isset($data['data']) || isset($data['errors']));
+
+        if (isset($data['data']) && $fields) {
+            $this->assertEquals($fields, array_keys(
+                isset($data['data']['items']) ? $data['data']['items'] : $data['data']
+            ));
+        }
     }
 
-    protected function assertJsonApiAuthResponse(JsonResponse $response)
+    /**
+     * @param JsonResponse | TestResponse $response
+     */
+    protected function assertJsonApiAuthResponse($response): void
     {
-        $data = $response->getData(true);
-        $this->assertArrayHasKey('access_token', $data['data']);
-        $this->assertArrayHasKey('token_type', $data['data']);
-        $this->assertArrayHasKey('expires_in', $data['data']);
+        $this->assertJsonApiResponse($response, [
+            'access_token',
+            'token_type',
+            'expires_in'
+        ]);
     }
 
-    protected function assertJsonApiResponseError(JsonResponse $response, string $error)
+    /**
+     * @param JsonResponse | TestResponse $response
+     * @param string $error
+     * @return bool
+     */
+    protected function assertJsonApiResponseError($response, string $error): void
     {
+        $response = $response instanceof TestResponse ? $response->baseResponse : $response;
+
         $data = $response->getData(true);
         $this->assertArrayHasKey('errors', $data);
         return in_array($error, $data['errors']);
