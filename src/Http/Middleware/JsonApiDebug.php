3b9e70a592143e0836201aef6ad2f8baab747dea
serhiikamolov <serhii.kamolov@gmail.com>
2021-11-09 23:54:47 +0200

    Return query log for all connections.

--- src/Http/Middleware/JsonApiDebug.php
+++ src/Http/Middleware/JsonApiDebug.php
@@ -8,6 +8,43 @@ use JsonAPI\Contracts\Response;
 
 class JsonApiDebug
 {
+    /**
+     * Enable query log for all connections
+     */
+    protected function enableQueryLog(): void
+    {
+        $connections = config('database.connections');
+        foreach ($connections as $connection => $params) {
+            try {
+                DB::connection($connection)->enableQueryLog();
+            } catch (\Exception $exception) {
+                continue;
+            }
+        }
+    }
+
+    /**
+     * Get query log from all connections
+     */
+    protected function getQueryLog(): array
+    {
+        $queryLog = [];
+        $total = 0;
+        $connections = config('database.connections');
+        foreach ($connections as $connection => $params) {
+            try {
+                if ($queries = DB::connection($connection)->getQueryLog()) {
+                    $queryLog[$connection] = $queries;
+                    $total = $total + sizeof($queries);
+                }
+            } catch (\Exception $exception) {
+                continue;
+            }
+        }
+
+        return [$total, $queryLog];
+    }
+
     public function handle($request, Closure $next)
     {
         if (!config('app.debug', false)) {
@@ -15,18 +52,23 @@ class JsonApiDebug
             return $response instanceof Response ? $response->unset('debug') : $response;
         }
 
-        DB::enableQueryLog();
+        $timeStart = microtime(true);
+
+        $this->enableQueryLog();
 
         /** @var Response $response */
         $response = $next($request);
 
+        $timeEnd = microtime(true);
+
         if ($response instanceof Response) {
-            $query = DB::getQueryLog();
+            list($total, $queries) = $this->getQueryLog();
             return $response->debug([
+                'time' => ($timeEnd - $timeStart),
                 'request' => $request->input() ?? [],
-                'queries' => [
-                    'total' =>  sizeof($query),
-                    'list' => $query
+                'database' => [
+                    'queries' => $total,
+                    'connections' => $queries
                 ]
             ]);
         }
