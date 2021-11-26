6778085d998e40bf17ca363d6524cedbcd6a7cef
serhiikamolov <serhii.kamolov@gmail.com>
2021-11-26 14:47:33 +0200

    Fix timestamp modifier

--- src/Traits/Serializer/Modifiers/Timestamp.php
+++ src/Traits/Serializer/Modifiers/Timestamp.php
@@ -3,20 +3,26 @@ namespace JsonAPI\Traits\Serializer\Modifiers;
 
 use Carbon\Carbon;
 use Carbon\CarbonImmutable;
+use Carbon\CarbonInterface;
 
 trait Timestamp
 {
     /**
      * @param string|null $date
-     * @return int|null
+     * @return mixed
      */
-    protected function modifierTimestamp(mixed $date): ?int
+    protected function modifierTimestamp(mixed $date): mixed
     {
-        if ($date instanceof CarbonImmutable) {
+        if ($date instanceof CarbonInterface) {
             return $date->timestamp;
         }
         if (is_string($date)) {
             return Carbon::parse($date)->timestamp;
+        }
+        if (is_array($date)) {
+            return array_map(function($date) {
+                return Carbon::parse($date)->timestamp;
+            }, $date);
         }
 
         return null;
